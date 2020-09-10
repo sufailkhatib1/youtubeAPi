b6726107c8c007bc49c486e80060560a09b35123
sufail khatib <sufailkhatib@icloud.com>
2020-09-10 13:06:07 +0200

    minor changes

---
+++ youtubeAPi/ConstantEdit.swift
@@ -0,0 +1,17 @@
+//
+//  ConstantEdit.swift
+//  youtubeAPi
+//
+//  Created by Sufail Khatib on 10/09/20.
+//  Copyright © 2020 Sufail Khatib. All rights reserved.
+//
+
+import Foundation
+// enter your API details here
+
+//struct Constants {
+//    static var API_KEY = ""
+//    static var PLAYLIST_ID = ""
+//    static var API_URL = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
+//}
+
